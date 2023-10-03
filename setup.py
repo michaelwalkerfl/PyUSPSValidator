5a9def326ca904441b1cea217437183a5e83f3a6
Michael Walker <michaelwalkerfl@users.noreply.github.com>
2023-10-02 21:00:25 -0400

    update: bump version in setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name="pyuspsvalidator",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/x-rst',
